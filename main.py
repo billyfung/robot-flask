83d2a9bf1c25c685d3453e4e39fc6bef57e273bd
Billy Fung <billy@billyfung.com>
2015-12-02 16:16:41 -0800

    cleaned up some functionality

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
-
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -29,17 +28,19 @@ def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
 
+@app.route('/<action>', methods = ['POST'])
+#this redirects commands from the html page which has buttons for actions
+def action(action):
+# #as per index.html, whenever the root url is requested, action() is called
+ #if subdirectory is called, we pass the name into the function robot/foo -> action('foo')
+    #if action == 'base-anti-clockwise':
+    	#then move arm by sending command via usb, and change status
+ 		#arm.MoveArm(t=1.0, cmd = 'base-anti-clockwise')
+#     if action == 'base-clockwise':
+# 	arm.MoveArm(t=1.0, cmd = 'base-clockwise')
+# and so on for the rest of the commands
+	response = {
+	'action' : 'Do the movement ' + action
+	}
 
-#@app.route('/<action>', methods = [‘POST’])
-# # #this redirects commands from the html page which has buttons for actions
-#def action(action):
-# # #as per index.html, when ever the root url is requested, action() is called
-# # #if subdirectory is called, we pass the name into the function robot/foo -> action(‘foo’)
-#     #if action == ‘base-anti-clockwise’:
-# # 	arm.MoveArm(t=1.0, cmd = ‘base-anti-clockwise’)
-# #     if action == ‘base-clockwise’:
-# # 	arm.MoveArm(t=1.0, cmd = ‘base-clockwise’)
-# # # and so on for the rest of the commands
-#	response = action
-
-#	return render_template('index.html', **response)
+	return render_template('index.html', **response)
